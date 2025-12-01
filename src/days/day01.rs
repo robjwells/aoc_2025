e7349ed2f63f89f522efd4ce564b4f305704d84e
Rob Wells <rob@robjwells.com>
2025-12-01 19:03:20 +0000

    Day 1 tidy up

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -10,8 +10,6 @@ use nom::{
 
 use crate::util::Answer;
 
-// static INPUT: &str = include_str!("../input/2025-01.txt");
-
 pub fn solve(input: &str) -> anyhow::Result<String> {
     let rotations = parse_input(input).expect("Failed to parse real input.");
     let (p1, p2) = times_at_zero(50, &rotations);
@@ -35,27 +33,26 @@ fn parse_input(s: &str) -> anyhow::Result<Vec<i16>> {
 }
 
 fn times_at_zero(start: i16, rotations: &[i16]) -> (i16, i16) {
-    let mut current = start;
+    let mut dial_position = start;
     let mut turning_zeroes = 0;
     let mut only_end_zeroes = 0;
     for rotation in rotations {
         turning_zeroes += rotation.abs() / 100;
-        let sub_100_delta = rotation % 100;
-
-        let next = current + sub_100_delta;
-
-        // Part two, count turning past zero.
-        // Checking current != 0 avoids double-counting when a rotation *starts* at zero.
-        if next <= 0 && current != 0 || next > 99 {
-            turning_zeroes += 1;
-        }
+        let next = dial_position + rotation % 100;
 
         // Part one, count only ending at zero.
         if next % 100 == 0 {
             only_end_zeroes += 1;
         }
 
-        current = match next {
+        // Part two, count turning past zero.
+        // Checking current != 0 avoids double-counting when a rotation starts at zero.
+        if next <= 0 && dial_position != 0 || next > 99 {
+            turning_zeroes += 1;
+        }
+
+        // Update current dial position with normalised next value.
+        dial_position = match next {
             ..0 => next + 100,
             0..=99 => next,
             100.. => next - 100,
